e3deaab90d507cb51d3b4b669700ed86b9d9ff85
Valérie Song <valerie.song@ormex.io>
2026-01-21 16:21:59 +0100

    feat: ajout messages erreurs login et register

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,7 +1,7 @@
-import {Component, inject} from '@angular/core';
-import {AuthService} from '../../../core/auth/services/auth-service';
-import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Router} from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { AuthService } from '../../../core/auth/services/auth-service';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,23 +12,43 @@ import {Router} from '@angular/router';
   styleUrl: './login.css',
 })
 export class Login {
-  private authService:AuthService = inject(AuthService)
+  private authService: AuthService = inject(AuthService)
   private fb = inject(FormBuilder)
   private router = inject(Router)
 
+  errorMessage = signal('')
+
   loginForm = this.fb.nonNullable.group({
     username: ['', Validators.required],
-    password: ['', [Validators.required, Validators.minLength(6)] ]
+    password: ['', [Validators.required, Validators.minLength(6)]]
   })
 
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
   onSubmit() {
+    this.errorMessage.set('')
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
     const credentials = this.loginForm.getRawValue()
     this.authService.login(credentials)
-      .subscribe(
-        (next) => {
+      .subscribe({
+        next: () => {
           this.router.navigate(['/tasks'])
         },
-      )
+        error: (err) => {
+          console.log(err)
+          this.errorMessage.set('Identifiants incorrects')
+        }
+      })
   }
 
 
