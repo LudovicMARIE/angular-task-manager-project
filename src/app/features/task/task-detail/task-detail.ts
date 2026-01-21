3c640b6c12795460be3ae441a0abd1b032f29cfb
Ludovic MARIE <91457982+LudovicMARIE@users.noreply.github.com>
2026-01-21 15:30:56 +0100

    feat: added details page

---
+++ src/app/features/task/task-detail/task-detail.ts
@@ -0,0 +1,41 @@
+import { DatePipe } from '@angular/common';
+import { Component, inject, Input, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Status, Tasks } from '../interfaces/tasks';
+import { TaskStatusPipe } from '../pipe/task-status-pipe';
+import { TaskService } from '../services/taskService';
+
+@Component({
+  selector: 'app-task-detail',
+  imports: [RouterLink, TaskStatusPipe, DatePipe],
+  templateUrl: './task-detail.html',
+  styleUrl: './task-detail.css',
+})
+export class TaskDetail implements OnInit {
+  private taskService = inject(TaskService);
+
+  @Input() id?: string;
+
+  task = signal<Tasks | undefined>(undefined);
+
+  ngOnInit(): void {
+    if (!this.id) return;
+    this.taskService.getTaskById(+this.id).subscribe({
+      next: (t: Tasks) => this.task.set(t),
+      error: () => console.log('error'),
+    });
+  }
+
+  getStatusColor(status: Status | undefined) {
+    switch (status) {
+      case Status.PENDING:
+        return 'bg-red-100 text-red-800';
+      case Status.IN_PROGRESS:
+        return 'bg-amber-100 text-amber-800';
+      case Status.DONE:
+        return 'bg-green-100 text-green-800';
+      default:
+        return 'bg-red-100 text-red-800';
+    }
+  }
+}
