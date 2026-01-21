8a204f482ff639561f878b8b48b5452dd20ebc22
Mathias002 <moussetmathias@gmail.com>
2026-01-21 23:56:00 +0100

    feat(): Ajout de la page dashboard
    Ajout de la page dashboard, protégée par les guards et implémentant des metrics simples comme le nombre de tâches totales, les tâches assignées au currentUser et une progress bar indiquant le pourcentage de tâches réalisées (affichage des metrics conditionné par le rôle du currentUser)

--- src/app/features/task/services/taskService.ts
+++ src/app/features/task/services/taskService.ts
@@ -1,31 +1,30 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../../../environments/environment';
-import {Observable} from 'rxjs';
-import {Tasks} from '../interfaces/tasks';
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { Tasks } from '../interfaces/tasks';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  private httpClient:HttpClient = inject(HttpClient)
-  private apiUrl:string = `${environment.apiUrl}/tasks`
+  private httpClient: HttpClient = inject(HttpClient);
+  private apiUrl: string = `${environment.apiUrl}/tasks`;
 
-  createTask (task: Tasks): Observable<Tasks> {
-    return this.httpClient.post<Tasks>(`${this.apiUrl}`, task)
-   }
+  createTask(task: Tasks): Observable<Tasks> {
+    return this.httpClient.post<Tasks>(`${this.apiUrl}`, task);
+  }
 
-   getAllTask (): Observable<Tasks[]> {
-    return this.httpClient.get<Tasks[]>(`${this.apiUrl}`)
-   }
+  getAllTask(): Observable<Tasks[]> {
+    return this.httpClient.get<Tasks[]>(`${this.apiUrl}`);
+  }
 
-   updateTask (id: number | undefined, task: Tasks): Observable<Tasks> {
-      return this.httpClient.patch<Tasks>(`${this.apiUrl}/${id}`, task)
-    }
+  updateTask(id: number | undefined, task: Tasks): Observable<Tasks> {
+    return this.httpClient.patch<Tasks>(`${this.apiUrl}/${id}`, task);
+  }
 
-    getTaskById (id: number | undefined): Observable<Tasks> {
-      return this.httpClient.get<Tasks>(`${this.apiUrl}/${id}`)
-    }
-
+  getTaskById(id: number | undefined): Observable<Tasks> {
+    return this.httpClient.get<Tasks>(`${this.apiUrl}/${id}`);
+  }
 
 }
